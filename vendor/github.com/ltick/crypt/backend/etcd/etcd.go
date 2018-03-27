67f9b2e5c2c112947a072229f39eda1b734d3850
Sam <sam42@outlook.com>
2018-03-27 22:30:06 +0800

    dep

--- vendor/github.com/ltick/crypt/backend/etcd/etcd.go
+++ vendor/github.com/ltick/crypt/backend/etcd/etcd.go
@@ -6,16 +6,15 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/ltick/crypt/backend"
-
 	goetcd "github.com/coreos/etcd/client"
+	"github.com/ltick/crypt/backend"
 )
 
 type Client struct {
 	client    goetcd.Client
 	keysAPI   goetcd.KeysAPI
 	waitIndex uint64
-	logger backend.Logger
+	logger    backend.Logger
 }
 
 func New(machines []string) (*Client, error) {
